93bae996b24ac6d764f5c3176a4d04ffc9495c77
Abhiram Inguva <admax1159@gmail.com>
2026-07-02 08:26:30 +0530

    Display List

--- Linked_list.py
+++ Linked_list.py
@@ -1,17 +1,8 @@
+from typing import dataclass_transform
 class node:
     def __init__(self,data):
         self.data = data
         self.next = None
-class list:
-    def __init__(self):
-        self.head = None
-    def display(self):
-        temp = self.head
-        if self.head == None:
-            print("Empty")
-        else:
-            while temp:
-                temp = temp.next
-                print(temp.data)
 
-        
+node1 = node(10)
+print(node1)
