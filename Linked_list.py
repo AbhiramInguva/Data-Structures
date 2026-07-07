787f4c1b7f61be4b65006ae383d520611aa2d585
Abhiram Inguva <admax1159@gmail.com>
2026-07-07 09:13:44 +0530

    Show element at index n

--- Linked_list.py
+++ Linked_list.py
@@ -1,8 +1,19 @@
-from typing import dataclass_transform
 class node:
     def __init__(self,data):
         self.data = data
         self.next = None
 
-node1 = node(10)
-print(node1)
+class list:
+    def __init__(self):
+        self.head = None
+    
+    def display_l(self):
+        if self.head is None:
+            print("List is empty")
+        else:
+            temp = self.head
+            while temp is not None:
+                print(temp.data)
+                temp = temp.next
+
+
